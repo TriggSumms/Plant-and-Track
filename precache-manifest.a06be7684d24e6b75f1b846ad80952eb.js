169cf59a7dcfae34ee988610152a8d3be0b0a713
TriggSumms <stsumms@gmail.com>
2020-08-23 18:31:33 -0500

    Updates

---
+++ precache-manifest.a06be7684d24e6b75f1b846ad80952eb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "883aae585758597a8f335af183746033",
+    "url": "/Plant-and-Track/index.html"
+  },
+  {
+    "revision": "6611f70c7e26702efa5f",
+    "url": "/Plant-and-Track/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "23dd1057940d286477e5",
+    "url": "/Plant-and-Track/static/css/main.183f1bbb.chunk.css"
+  },
+  {
+    "revision": "6611f70c7e26702efa5f",
+    "url": "/Plant-and-Track/static/js/2.6f93f332.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Plant-and-Track/static/js/2.6f93f332.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23dd1057940d286477e5",
+    "url": "/Plant-and-Track/static/js/main.d2a4cf14.chunk.js"
+  },
+  {
+    "revision": "b94ac61888a0496852e5",
+    "url": "/Plant-and-Track/static/js/runtime-main.37f39871.js"
+  },
+  {
+    "revision": "8cc1a2a2b6d77a71d2edc8bd963d01bb",
+    "url": "/Plant-and-Track/static/media/login.8cc1a2a2.svg"
+  },
+  {
+    "revision": "d7ed901f438acef692fe3412a2963de0",
+    "url": "/Plant-and-Track/static/media/plantBanner.d7ed901f.jpg"
+  }
+]);
